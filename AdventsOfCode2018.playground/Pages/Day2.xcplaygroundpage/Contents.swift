483acc9926da7e8c5ed83e1894d57c5179a6e530
Remus Lazar <remus@lazar.info>
2018-12-09 13:17:35 +0100

    Day 2, Part 2

--- AdventsOfCode2018.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ AdventsOfCode2018.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -15,4 +15,19 @@ let numOfBoxesWith3Chars = boxes.filter { $0.charsWhichOccurr(times: 3).count >=
 
 numOfBoxesWith2Chars * numOfBoxesWith3Chars
 
+// Part 2
+
+func findBoxes() -> (Box,Box)? {
+    for box in boxes {
+        if let found = boxes.first(where: { $0.numberOfDifferingChars(comparing: box) == 1 }) {
+            return (found,box)
+        }
+    }
+    return nil
+}
+
+if let boxes = findBoxes() {
+    boxes.0.nonDifferingChars(comparing: boxes.1)
+}
+
 //: [Next](@next)
--- AdventsOfCode2018.playground/Pages/Day2.xcplaygroundpage/Sources/Box.swift
+++ AdventsOfCode2018.playground/Pages/Day2.xcplaygroundpage/Sources/Box.swift
@@ -25,3 +25,27 @@ extension Box {
         return id.filter { $0 == character }.count
     }
 }
+
+extension Box {
+    public func numberOfDifferingChars(comparing to: Box) -> Int {
+        var diffCount = 0
+        for index in id.indices {
+            if self.id[index] != to.id[index] { diffCount += 1 }
+        }
+        return diffCount
+    }
+    
+    public func nonDifferingChars(comparing to: Box) -> String {
+        var chars = ""
+        for index in id.indices {
+            if self.id[index] == to.id[index] { chars += String(self.id[index]) }
+        }
+        return chars
+    }
+}
+
+extension Box: CustomStringConvertible {
+    public var description: String {
+        return id
+    }
+}
